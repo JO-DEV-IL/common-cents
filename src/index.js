db6c962a68a1c9f519f17fe9fc59e27c28cbb272
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-10-27 12:16:47 -0400

    updated react render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client'
 import App from "./App"
 
-ReactDOM.render(
+//React bootstrap library
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//ReactDom.render NO LONGER SUPPORTED!!
+//Below is new code to render app to root
+//new import is: import { createRoot } from 'react-dom/client'
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
